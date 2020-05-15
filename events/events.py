afd88e32f95a32c395c437f048601fcfb512cfd0
Kerati <enderwolf50@gmail.com>
2020-05-15 17:39:56 +0800

    cog update

---
+++ events/events.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import random
+import json
+from core.classes import Cog_Ext
+
+with open("others.json", "r", encoding= "utf8") as jothers:
+    other = json.load(jothers)
+
+class Events(Cog_Ext):
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot is ready.")
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if "窩不知道" in msg.content:
+            await msg.channel.send(random.choice(other["IDK_url"]))
+        await self.bot.process_commands(msg)
+
+def setup(bot):
+    bot.add_cog(Events(bot))
--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import os
 
 bot = commands.Bot(command_prefix= ".")
 
-with open("others.json", 'r', encoding= "utf8") as jothers:
-    other = json.load(jothers)
-
 @bot.command()
 async def load(ctx, extension):
     bot.load_extension(f"cmds.{extension}")
@@ -24,20 +21,13 @@ async def reload(ctx, extension):
     bot.reload_extension(f"cmds.{extension}")
     await ctx.send(f"**{extension}** has been reloaded!")
 
-@bot.event
-async def on_ready():
-    print("Bot is ready.")
-
-@bot.event
-async def on_message(msg):
-    if "窩不知道" in msg.content:
-        await msg.channel.send(random.choice(other["IDK_url"]))
-    await bot.process_commands(msg)
-
-
 for Filename in os.listdir("./cmds"):
     if Filename.endswith(".py"):
         bot.load_extension(F"cmds.{Filename[:-3]}")
+
+for Filename in os.listdir("./events"):
+    if Filename.endswith(".py"):
+        bot.load_extension(f"events.{Filename[:-3]}")
 
 if __name__ == "__main__":
     bot.run("NzEwNDk4MDg0MTk0NDg0MjM1.Xr1WWQ.H0OcO7XyHrderg6rCVJFfiWEZiA")
