011909daa479fe7d2be3b5da12a179adc6a71faf
Kerati <enderwolf50@gmail.com>
2020-05-17 23:51:02 +0800

    1A2B game complete?

--- cmds/1A2B.py
+++ cmds/1A2B.py
@@ -5,9 +5,9 @@ import random
 
 AB_G = False
 wait = False
-End = False
 Duplicate = False
 channel = ""
+Answer = ""
 Number = ""
 Number_Guessed = ""
 
@@ -16,20 +16,21 @@ class _1A2B(Cog_Ext):
     async def AB_G(self, ctx):
         global AB_G
         global channel
-        global Number
+        global Answer
         if AB_G == False:
             AB_G = True
             channel = ctx.channel
-            Number = random.sample('1234567890', 4)
-            
+            Answer = random.sample('1234567890', 4)
+
     @commands.command()
-    async def g(self, ctx, num):
+    async def AB_E(self, ctx):
         global AB_G
-        global Number_Guessed
-        global channel
-        if AB_G == True:
-            Number_Guessed = list(num)
-            await ctx.send(Number_Guessed)
+        global Answer
+        global Number
+        AB_G = False 
+        Number = "".join(Answer)
+        await channel.send(ctx.author.mention + " 結束了遊戲！")
+        await channel.send("正確答案為： " + f"**{Number}**！")
 
     @commands.Cog.listener()
     async def on_message(self, msg):
@@ -37,8 +38,7 @@ class _1A2B(Cog_Ext):
         global channel
         global wait
         global Number_Guessed
-        global Number
-        global End
+        global Answer
         global Duplicate
         if msg.channel == channel:
             if AB_G == True:
@@ -46,7 +46,7 @@ class _1A2B(Cog_Ext):
                     wait = True
                     await channel.send("請輸入四位不同數字：")
                 else:
-                    if msg.content.isdigit() == True and len(msg.content) ==4:
+                    if msg.content.isdigit() == True and len(msg.content) == 4:
                         Number_Guessed = list(msg.content)
                         j = 0
                         while j < 4:
@@ -56,7 +56,8 @@ class _1A2B(Cog_Ext):
                                     k += 1
                                     continue
                                 if Duplicate == True:
-                                    await channel.send("No!")
+                                    j = 4
+                                    k = 4
                                 else:
                                     if (Number_Guessed[j] == Number_Guessed[k]):
                                         Duplicate = True
@@ -65,7 +66,7 @@ class _1A2B(Cog_Ext):
                         if Duplicate == False:
                             A = 0
                             for i in range(4):
-                                if (Number_Guessed[i] == Number[i]):
+                                if (Number_Guessed[i] == Answer[i]):
                                     A += 1
                             B = 0
                             j = 0
@@ -75,7 +76,7 @@ class _1A2B(Cog_Ext):
                                     if j == k:
                                         k += 1
                                         continue
-                                    if (Number_Guessed[j] == Number[k]):
+                                    if (Number_Guessed[j] == Answer[k]):
                                         B += 1
                                     k += 1
                                 j += 1
@@ -87,39 +88,10 @@ class _1A2B(Cog_Ext):
                             else:
                                 wait = False
                                 await channel.send(msg.author.mention + f"（{msg.content}）" + f"：**{A}A{B}B**")
+                        else:
+                            await channel.send(msg.author.mention + " 請勿輸入重複的數字！")
+                            Duplicate = False
 
 
 def setup(bot):
     bot.add_cog(_1A2B(bot))
-
-"""
-import random
-x=random.sample('1234567890',4)
-print (x)
-play=True
-while play:
-    y=input("輸入4個不同數字:")
-    print (y)
-    z = list(y)
-    print (z)
-    a=0
-    for i in range(4):
-        if(x[i]==z[i]):
-            a=a+1
-    b=0
-    j=0
-    while j < 4:
-        k=0
-        while k < 4:
-            if j==k :
-                k=k+1
-                continue
-            if(x[j]==z[k]):
-                b=b+1
-            k=k+1
-        j=j+1
-     
-    print(a,"A", b, "B")
-    if a == 4:
-        play = False
-"""
