c25e2193f13ddb25e09c68f413cbdf1d0335bd9e
Kerati <enderwolf50@gmail.com>
2020-06-03 21:23:31 +0800

    roll

---
+++ cmds/roll.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Ext
+import random
+import json
+
+class Roll(Cog_Ext):
+    @commands.command()
+    async def roll(self, ctx, num: int, *,arg= "{}"):
+        if num <= 0:
+            await ctx.send("ㄐㄐ")
+            await ctx.send("雞雞")    
+        else:
+            await ctx.send(arg.format(random.randrange(1, num + 1)))
+        # else:
+        #     with open("others.json", "r", encoding= "utf-8") as jothers:
+        #         other = json.load(jothers)
+        #     await ctx.send(other["Roll Message"].format(random.randrange(1, num + 1)))
+
+    # @commands.command()
+    # async def setrm(self, ctx, *,arg):
+    #     if ctx.author == self.bot.get_user(590430031281651722) or ctx.author == self.bot.get_user(523755296242270210):
+    #         with open("others.json", "r", encoding= "utf-8") as jothers:
+    #             other = json.load(jothers)
+
+    #             other["Roll Message"] = arg
+
+    #         with open("others.json", "w", encoding= "utf-8") as jothers:
+    #             json.dump(other, jothers)
+
+    #         await ctx.send("訊息已設定為 __" + other["Roll Message"] + "__")
+
+    @commands.command()
+    async def bzz(self, ctx):
+        await ctx.send(ctx.author.mention + "：" + str(random.choice(["大凶", "小凶", "凶", "平", "吉", "小吉", "大吉", "吉掰", "大吉掰"])))
+
+def setup(bot):
+    bot.add_cog(Roll(bot))
