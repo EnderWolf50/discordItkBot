6ed5ccedaf50d0a8f36674b605e87dc5a26d4936
EnderWolf50 <enderwolf50@gmail.com>
2020-07-05 00:03:03 +0800

    Create main_Heroku.py

---
+++ main_Heroku.py
@@ -0,0 +1,27 @@
+import discord
+import os
+from discord.ext import commands
+
+TOKEN = os.environ["TOKEN"]
+
+bot = commands.Bot(command_prefix=".", case_insensitive=True)
+bot.remove_command("help")
+
+for Filename in os.listdir("./cmds"):
+    if Filename.endswith(".py"):
+        bot.load_extension(f"cmds.{Filename[:-3]}")
+
+for Filename in os.listdir("./events"):
+    if Filename.endswith(".py"):
+        bot.load_extension(f"events.{Filename[:-3]}")
+
+for Filename in os.listdir("./games"):
+    if Filename.endswith(".py"):
+        bot.load_extension(f"games.{Filename[:-3]}")
+
+for Filename in os.listdir("./tasks"):
+    if Filename.endswith(".py"):
+        bot.load_extension(f"tasks.{Filename[:-3]}")
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
