bcefdd34d5d2eba43e13d5d33cc8a83987603a2e
EnderWolf50 <enderwolf50@gmail.com>
2020-07-10 02:26:13 +0800

    Add files via upload

--- cmds/choose.py
+++ cmds/choose.py
@@ -4,14 +4,12 @@ from core.classes import Cog_Ext
 
 import random
 
-
 class Choose(Cog_Ext):
     @commands.command()
-    async def choose(self, ctx, *, arg):
+    async def choose(self, ctx, *,arg):
         list = arg.split(" ")
         await ctx.author.send(random.choice(list))
-        await ctx.message.delete(delay=3)
-
+        await ctx.message.delete(delay= 3)
 
 def setup(bot):
     bot.add_cog(Choose(bot))
