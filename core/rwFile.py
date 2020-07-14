1f5e9602cf7e69fff6037396b7c5adb215be097f
Kerati <enderwolf50@gmail.com>
2020-07-14 17:52:40 +0800

    .

--- core/rwFile.py
+++ core/rwFile.py
@@ -2,10 +2,10 @@ import json
 
 def get_setting(Data=None):
     if Data == None:
-        with open("setting.json", "r", encoding="utf8") as jsettings:
+        with open("settings.json", "r", encoding="utf8") as jsettings:
             return json.load(jsettings)
     else:
-        with open("setting.json", "r", encoding="utf8") as jsettings:
+        with open("settings.json", "r", encoding="utf8") as jsettings:
             return json.load(jsettings)[Data]
 
 
