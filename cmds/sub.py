fc0577737c7ad2f8b05712e52b7c033d4760d52a
EnderWolf50 <enderwolf50@gmail.com>
2020-07-17 01:54:19 +0800

    Update sub.py

--- cmds/sub.py
+++ cmds/sub.py
@@ -254,9 +254,8 @@ class Subscribe(Cog_Ext):
     async def test(self, ctx, ID: int):
         Msg = await ctx.fetch_message(ID)
         embed = Msg.embeds[0]
-        embed.add_field(name="test", value="foo")
-        await ctx.send(Msg.jump_url)
-        await Msg.edit(embed= embed)
+        description = embed.description.split("\n")
+        await ctx.send(description)
 
 def setup(bot):
     bot.add_cog(Subscribe(bot))
