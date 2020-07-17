404d5a990d73ab5167f54b7787409df73714ff1c
Kerati <enderwolf50@gmail.com>
2020-07-17 15:16:40 +0800

    Update events.py

--- events/events.py
+++ events/events.py
@@ -19,17 +19,16 @@ class Events(Cog_Ext):
     async def on_message(self, msg):
         global url
         global url_weights
+        if msg.author.bot: return
         if "窩不知道" in msg.content or "idk" in msg.content.lower():
-            if msg.author != self.bot.user:
+            File = rFile("others")["IDK_url"]
 
-                File = rFile("others")["IDK_url"]
+            Picture = str(random.choices(list(File.keys()), weights= list(File.values()))).strip("[]'")
 
-                Picture = str(random.choices(list(File.keys()), weights= list(File.values()))).strip("[]'")
-
-                if Picture == "https://i.imgur.com/x1qmYCT.gif":
-                    await msg.channel.send(Picture, delete_after= 18.68)
-                else:
-                    await msg.channel.send(Picture, delete_after= 5)
+            if Picture == "https://i.imgur.com/x1qmYCT.gif":
+                await msg.channel.send(Picture, delete_after= 18.68)
+            else:
+                await msg.channel.send(Picture, delete_after= 5)
         elif msg.content == "ㄐㄐ":
             await msg.add_reaction("\N{AUBERGINE}")
         elif msg.content == "雞雞":
