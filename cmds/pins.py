1d42d9eceab9a782703764ef7ca9ddc7a5fd4bdd
Kerati <enderwolf50@gmail.com>
2020-07-31 19:38:27 +0800

    Create pins.py

---
+++ cmds/pins.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Ext
+
+import random
+
+
+class Pins(Cog_Ext):
+    @commands.command(aliases=['pin'])
+    async def pins(self, ctx):
+        msg = await ctx.fetch_message(random.choice(await ctx.channel.pins()).id)
+        msgContent = msg.content
+        msgAuthor = msg.author.id
+        await ctx.send(f"<@{msgAuthor}>：{msgContent}")
+
+
+def setup(bot):
+    bot.add_cog(Pins(bot))
