afe1d2ea19afc54643cb436e809d3c419ac72691
EnderWolf50 <enderwolf50@gmail.com>
2020-08-08 18:00:19 +0800

    Update events.py

--- events/events.py
+++ events/events.py
@@ -95,7 +95,7 @@ class Events(Cog_Ext):
         if msg.content[1:].split(' ')[0] in self.cmdList: return
 
         await self.bot.get_channel(741556551143391323).send(
-            f'{msg.author.mention}  `{dt.now().strftime("%Y/%m/%d %H:%M:%S")}`\n{msg.content}'
+            f'{msg.author.display_name}  `{dt.now().strftime("%Y/%m/%d %H:%M:%S")}`\n{msg.content}'
         )
         for attachment in msg.attachments:
             await self.bot.get_channel(741556551143391323).send(
