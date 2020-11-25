91b94ec44de072898e464f99f2831cb00857932c
Kerati <enderwolf50@gmail.com>
2020-11-25 15:11:11 +0800

    Cue update

--- cmds/cue.py
+++ cmds/cue.py
@@ -7,28 +7,48 @@ import random
 words_list = [
     '加母鴨', '拉大筋天', '路滷味', '大艙酷', '麻爛郭', '拋山怪', '我錯了 請綁我(?', '原來有烈恆', '鯊鯊把我下吃了',
     '雌', '捏褲', '速鞭', '後麵', '麻將乾麵', '股溝', '像練', '可惜恐嚇 可口可樂', '可惜恐嚇 可口可樂',
-    'vul4n3', '韓式炸肉狗'
+    'vul4n3', '韓式炸肉狗', '找一步', '溏心但', '借魚頭', '毒水', '性啥', '綠嘎理'
 ]
+
+prev_msg = None
+curr_msg = None
 
 
 class Cue(Cog_Ext):
     @commands.command()
-    async def Cue(self, ctx):
+    async def Cue(self, ctx, pos: int = None):
+        if pos:
+            await ctx.send(
+                f'<@!429992095374114826> 語錄 {pos} - {words_list[pos - 1]}')
+            return
         word = random.choice(words_list)
         await ctx.send(
-            f'<@!429992095374114826> 語錄 {words_list.index(word)+1} - {word}')
+            f'<@!429992095374114826> 語錄 {words_list.index(word) + 1} - {word}')
 
     @commands.command()
     async def Cue_add(self, ctx, *, word):
         words_list.append(word)
-        await self.bot.get_channel(725295821456801845).send(words_list)
+        await ctx.send(f'已成功增加語錄 {len(words_list)} - {word}')
+
+        curr_msg = await self.bot.get_channel(725295821456801845).send(
+            words_list)
+        global prev_msg
+        if prev_msg:
+            await prev_msg.delete()
+            prev_msg = curr_msg
 
     @commands.command()
     async def Cue_del(self, ctx, num: int):
         word_temp = words_list[num - 1]
         del words_list[num - 1]
         await ctx.send(f'已成功刪除語錄 {num} - {word_temp}')
-        await self.bot.get_channel(725295821456801845).send(words_list)
+
+        curr_msg = await self.bot.get_channel(725295821456801845).send(
+            words_list)
+        global prev_msg
+        if prev_msg:
+            await prev_msg.delete()
+            prev_msg = curr_msg
 
     @commands.command()
     async def Cue_list(self, ctx):
