f0671bad8a5af531f8f1156e756e78fc0229fe25
Kerati <enderwolf50@gmail.com>
2021-01-20 15:43:40 +0800

    Remove PBT and SUS

--- events/events.py
+++ events/events.py
@@ -41,55 +41,6 @@ loadingCatEmos = [
     "<:ldc_11:725343129087115374><:ldc_12:725343128818548737><:ldc_13:725343128822743041><:ldc_14:725343128994840657><:ldc_15:725343129003360346>"
 ]
 
-voted_messages = []
-
-au_emojis = [
-    '<:AU_yellow:773465852044640286>', '<:AU_white:773465852015804446>',
-    '<:AU_red:773465851969798154>', '<:AU_purple:773465851705688065>',
-    '<:AU_pink:773465852283715584>', '<:AU_orange:773465851873591296>',
-    '<:AU_lime:773465851809497128>', '<:AU_green:773465851717353472>',
-    '<:AU_cyan:773465851713421312>', '<:AU_brown:773465851453243423>',
-    '<:AU_blue:773465851709358111>', '<:AU_black:773465851634122752>'
-]
-
-au_messages = [
-    '''.　　 。　　               　　　 　 。 . 　     　 • 　　　   　•
-
-　. 　ﾟ　.        {}  was An Impostor.　            。   　.
-
-　   　'　　          　  0 Impostor remains      　 　　   。''', '''
-.　　 。　ඞ　               　　　  。 . 　     　 • 　　　   　•
-
-　. 　ﾟ　.        {}  was An Impostor.　            。   　.
-
-　   　'　　          　  0 Impostor remains      　 　　   。''', '''
-.　　 。　　           ඞ    　　　  。 . 　     　 • 　　　   　•
-
-　. 　ﾟ　.        {}  was An Impostor.　            。   　.
-
-　   　'　　          　  0 Impostor remains      　 　　   。''', '''
-.　　 。　　               　　　 ඞ 。 . 　     　 • 　　　   　•
-
-　. 　ﾟ　.        {}  was An Impostor.　            。   　.
-
-　   　'　　          　  0 Impostor remains      　 　　   。''', '''
-.　　 。　　               　　　 　 。 . 　 ඞ     • 　　　   　•
-
-　. 　ﾟ　.        {}  was An Impostor.　            。   　.
-
-　   　'　　          　  0 Impostor remains      　 　　   。''', '''
-.　　 。　　              　　　　   。 . 　     　 • 　　ඞ   　•
-
-　. 　ﾟ　.        {}  was An Impostor.　            。   　.
-
-　   　'　　          　  0 Impostor remains      　 　　   。.''', '''
-.　　 。　　               　　　 　 。 . 　     　 • 　　　   　•
-
-　. 　ﾟ　.        {}  was An Impostor.　            。   　.
-
-　   　'　　          　  0 Impostor remains      　 　　   。'''
-]
-
 
 class Events(Cog_Ext):
     def __init__(self, *args, **kwargs):
@@ -211,33 +162,6 @@ class Events(Cog_Ext):
                 await msg.channel.send(random.choice(actCute))
             else:
                 await msg.channel.send(f"還敢撒嬌阿 {msg.author.mention}")
-        # HUA_HUA sus
-        if msg.author.id == 429992095374114826:
-            await msg.add_reaction(random.choice(['🎫', '🎟️']))
-        # PBT
-        PBT = 1 if msg.author.id == 591657649762861111 else 2 if msg.author.id == 343008920748425217 else 0
-        if PBT:
-            send_pic = 0
-            async for h_msg in msg.channel.history(limit=20,
-                                                   after=msg.created_at -
-                                                   timedelta(seconds=10)):
-                if h_msg.author.id == 343008920748425217 and PBT == 1:
-                    send_pic = 1
-                elif h_msg.author.id == 591657649762861111 and PBT == 2:
-                    send_pic = 1
-
-                for a in h_msg.attachments:
-                    if a.filename == "play_big_two.jpg":
-                        send_pic = 2
-                        break
-                if send_pic == 2: break
-            if send_pic == 1:
-                Pic = random.choice([
-                    'play_big_two.jpg', 'play_big_two_1.gif',
-                    'play_big_two_2.gif'
-                ])
-                await msg.channel.send(file=discord.File(f'./images/{Pic}'),
-                                       delete_after=10)
 
     @commands.Cog.listener()
     async def on_message_delete(self, msg):
@@ -271,31 +195,6 @@ class Events(Cog_Ext):
                 await reaction.message.add_reaction("\N{THUMBS DOWN SIGN}")
         if "ldcat" in reaction.message.content:
             await reaction.message.remove_reaction(reaction, user)
-        if reaction.emoji in ['🎫', '🎟️']:
-            if reaction.count == 3 and reaction.message.id not in voted_messages:
-                voted_messages.append(reaction.message.id)
-                await reaction.message.clear_reactions()
-
-                emojis = au_emojis.copy()
-                for i in range(3):
-                    random.shuffle(emojis)
-                    await reaction.message.add_reaction(emojis.pop())
-                    await asyncio.sleep(0.5)
-
-                await asyncio.sleep(2)
-                await reaction.message.clear_reactions()
-
-                msg = await reaction.message.channel.send(
-                    au_messages[0].format(reaction.message.author.mention))
-                for au_msg in au_messages[1:-1]:
-                    await asyncio.sleep(1)
-                    await msg.edit(
-                        content=au_msg.format(reaction.message.author.mention))
-
-                await asyncio.sleep(1)
-                await msg.edit(content=au_messages[-1].format(
-                    reaction.message.author.mention),
-                               delete_after=7)
 
     @commands.Cog.listener()
     async def on_reaction_remove(self, reaction, user):
