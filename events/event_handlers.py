b898379ba8afc30496ed03b90c0fdfea86f6c7e3
enderwolf50 <enderwolf50@gmail.com>
2021-05-08 03:14:17 +0800

    我不懂 generator= =

--- events/event_handlers.py
+++ events/event_handlers.py
@@ -105,7 +105,7 @@ class EventHandlers(CogInit):
             pic = discord.File(Events.so_hot)
             await msg.reply(file=pic, delete_after=7)
         # 素每（名稱）
-        elif "素每" in (u.display_name for u in msg.mentions):
+        elif "素每" in [u.display_name for u in msg.mentions]:
             pic = discord.File(Events.so_hot)
             await msg.reply(file=pic, delete_after=7)
         # 好耶
