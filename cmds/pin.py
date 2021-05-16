974ad027ba9192a9402a9d0e1a9ce20df167a75c
enderwolf50 <enderwolf50@gmail.com>
2021-05-16 21:42:47 +0800

    小改

--- cmds/pin.py
+++ cmds/pin.py
@@ -1,40 +1,54 @@
 import random
+from datetime import datetime as dt
+from datetime import timedelta
 
 import discord
-from core import CogInit
+from core import Bot, CogInit
 from discord.ext import commands
 
 
 class Pin(CogInit):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    async def _update_pins(self) -> None:
+        self._pins = await self.bot.get_channel(Bot.main_channel).pins()
+        self._last_update = dt.utcnow()
+
+    @commands.Cog.listener()
+    async def on_ready(self) -> None:
+        await self._update_pins()
+
     @commands.command()
     async def pin(self, ctx: commands.Context, user: discord.Member = None) -> None:
+        random_pin: discord.Message = None
+        author: discord.Member = None
+        content: str = None
+
         if user is None:
             await ctx.message.delete(delay=3)
-            random_pin = await ctx.fetch_message(
-                random.choice(await ctx.channel.pins()).id
-            )
+            # 檢查並更新 pin 列表
+            if dt.utcnow() - self._last_update >= timedelta(minutes=5):
+                await self._update_pins()
 
+            random_pin = random.choice(self._pins)
             author = random_pin.author.display_name
             content = random_pin.content
-            if len(random_pin.attachments) == 0:
-                await ctx.send(f"{author}：\n{content}")
-            else:
-                for attachment in random_pin.attachments:
-                    await ctx.send(f"{attachment.url}")
         else:
             await ctx.message.delete(delay=3)
-            author_pin_list = [
-                m for m in (await ctx.channel.pins()) if m.author.id == user.id
-            ]
-            random_pin = await ctx.fetch_message(random.choice(author_pin_list).id)
+            # 檢查並更新 pin 列表
+            if dt.utcnow() - self._last_update >= timedelta(minutes=5):
+                await self._update_pins()
 
+            random_pin = random.choice([m for m in self._pins if m.author == user])
             author = random_pin.author.display_name
             content = random_pin.content
-            if len(random_pin.attachments) == 0:
-                await ctx.send(f"{author}：\n{content}")
-            else:
-                for attachment in random_pin.attachments:
-                    await ctx.send(f"{attachment.url}")
+
+        if not random_pin.attachments:
+            await ctx.send(f"{author}：\n{content}")
+        else:
+            for attachment in random_pin.attachments:
+                await ctx.send(f"{attachment.url}")
 
 
 def setup(bot) -> None:
